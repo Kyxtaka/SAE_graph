f1c5fc559c23301e923088f9cdf3cb9a084d8c05
Nathan Randriantsoa <randriantsoanathan1@gmail.com>
2024-04-16 14:45:05 +0200

    Initialisation de oracle.py

---
+++ src/oracle.py
@@ -0,0 +1,13 @@
+#fichier où seront implémenter les requêtes python
+#import 
+import json
+
+
+#constant
+json_file_dir = ""
+#function
+def load_json(path):
+    return 
+
+if __name__ == "__main__" :
+    print("Hello World")
