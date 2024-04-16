80849eb05c57ea80592adf64016a5006c14e257d
Nathan Randriantsoa <randriantsoanathan1@gmail.com>
2024-04-16 14:57:48 +0200

    initialisation de requetes + definition des methode en  TDD (sans code)

---
+++ src/requetes.py
@@ -0,0 +1,39 @@
+#fichier où seront implémenter les requêtes python
+import json
+import networkx as nx
+
+#Q1
+def json_ver_nx():
+    ...
+
+#Q2
+def collaborateurs_communs():
+    ...
+
+#Q3 
+def collaborateurs_proches():
+    ...
+
+def est_proche():
+    ...
+
+def distance_naive():
+    ...
+
+def distance():
+    ...
+
+#Q4
+def centralite():
+    ...
+
+def centre_hollywood():
+    ...
+
+#Q5
+def eloignement_max():
+    ...
+
+#Bonus
+def centralite_groupe():
+    ...
