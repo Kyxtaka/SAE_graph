d5fa1a010bc2000b2a68dafdf70e8b2dc272cee2
Pierre Gangneux <pierre.gangneux@etu.univ-orleans.fr>
2024-05-06 11:25:02 +0200

    ajout de collaborateurs_proches

--- src/requetes.py
+++ src/requetes.py
@@ -37,8 +37,28 @@ def collaborateurs_communs(graph, acteur1, acteur2):
     return ens_commun
 
 #Q3 
-def collaborateurs_proches():
-    ...
+def collaborateurs_proches(G,u,k):
+    """Fonction renvoyant l'ensemble des acteurs à distance au plus k de l'acteur u dans le graphe G. La fonction renvoie None si u est absent du graphe.
+    
+    Parametres:
+        G: le graphe
+        u: le sommet de départ
+        k: la distance depuis u
+    """
+    if u not in G.nodes:
+        print(u,"est un illustre inconnu")
+        return None
+    collaborateurs = set()
+    collaborateurs.add(u)
+    print(collaborateurs)
+    for i in range(k):
+        collaborateurs_directs = set()
+        for c in collaborateurs:
+            for voisin in G.adj[c]:
+                if voisin not in collaborateurs:
+                    collaborateurs_directs.add(voisin)
+        collaborateurs = collaborateurs.union(collaborateurs_directs)
+    return collaborateurs
 
 def est_proche():
     ...
