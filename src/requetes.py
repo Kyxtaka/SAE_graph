b693894234b6da5c5afcae10a87b6ebd06bccc7d
Pierre Gangneux <pierre.gangneux@etu.univ-orleans.fr>
2024-05-21 13:55:01 +0200

    opti question1

--- src/requetes.py
+++ src/requetes.py
@@ -5,32 +5,7 @@ import itertools
 
 
 #Q1
-def json_ver_nx(chemin:str) -> nx.Graph:
-    json_file = []
-    with open(chemin, mode="r", encoding="utf-8") as file:
-        for line in file:
-            json_file.append(json.loads(line))
-    actors_list = set()
-    actors_edges = set()
-    for film in json_file:
-        tmp_actors = film['cast']
-        for i in range(len(tmp_actors)):
-            for k in range(i+1,len(tmp_actors)):
-                actorName_1 = str(tmp_actors[i])
-                actorname_2 = str(tmp_actors[k])
-                actorName_1 = actorName_1.translate({ord(i): None for i in "[]'"})
-                actorname_2 = actorname_2.translate({ord(i): None for i in "[]'"})
-                actors_list.add(actorName_1)
-                actors_edges.add((actorName_1,actorname_2))
-    #print(actors_edges)
-    # print(len(actors))
-    print(len(actors_edges))
-    Graph = nx.Graph()
-    Graph.add_edges_from(actors_edges)
-    print(len(Graph.nodes))
-    return Graph
-
-def json_ver_nx2(chemin:str):
+def json_ver_nx(chemin:str):
     with open(chemin, mode="r", encoding="utf-8") as file:
         G = nx.Graph()
         for line in file:
