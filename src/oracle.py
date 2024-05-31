7f93d1e0aaabd1002e8e47e3a89c2da4121de1f2
Nathan Randriantsoa <134612654+Kyxtaka@users.noreply.github.com>
2024-05-31 23:44:47 +0200

    Oracle app (#29)
    * ajout de debut GUI tkinter
    
    * modification : ajout de la signature des différentes fonction a implémenter
    
    * edit test
    
    * ajout des bouton dans le sideBar de l'interface graphique

--- src/oracle.py
+++ src/oracle.py
@@ -27,36 +27,63 @@ def getTextEntryInput(textField:Entry) -> str:
 #Implementation des requetes en affichage graphique (GUI)
 #Toutes les les requtes se verront affichier une boites de dialogue affichant la réponse a sa fonction
 def showActorsCommonComllaborators(): #doit affichier une fentre de dialogue
-    ...
+    print("Collaborateurs Commun")
 
 def showActorNearestCollaboratorsK():
-    ...
+    print("Collaborateurs proch K")
 
 def showIfActorNearestCollaboratorK():
-    ...
+    print("Est proche K")
 
 def showActorsDistance():
-    ...
+    print("Distance")
 
 def showActorCentrality():
-    ...
+    print("Centralite")
 
 def showHollywoodCenter():
-    ...
+    print("Centre Hollywood")
 
 def showMaxDistance():
-    ...
+    print("Eloignement Max")
 
 # interface graphique
-def leftPanel(root:Tk):
+def leftPanel():
     # relief='sunken'
     sidebar = Frame(root, width=200, bg='#CCC', height=500,borderwidth=2)
 
     # A compléter
+    #Collaborateur Communs
+    actorCommonCollabButton = Button(sidebar, text="Collaborateurs Communs", command=showActorsCommonComllaborators)
+    actorCommonCollabButton.pack(padx=(20),pady=(20))
+
+    #Collaborateur proche en k
+    actorNearCollabKButton = Button(sidebar, text="Collaborateurs Proches en k", command=showIfActorNearestCollaboratorK)
+    actorNearCollabKButton.pack(padx=(20),pady=(20))
+    
+    #Est proche en K
+    actorIfNearCollabKButton = Button(sidebar, text="Est Proches en k", command=showActorNearestCollaboratorsK)
+    actorIfNearCollabKButton.pack(padx=(20),pady=(20))
+
+    #Distance entre deux acteurs
+    actorDistanceButton = Button(sidebar, text="Distance entre 2 Acteurs", command=showActorsDistance)
+    actorDistanceButton.pack(padx=(20),pady=(20))
+
+    #Centralite d'un acteur
+    actorCentralityButton = Button(sidebar, text="Centralite Acteur", command=showActorCentrality)
+    actorCentralityButton.pack(padx=(20),pady=(20))
+
+    #Centre Hollywood
+    actorCentreHollwoodButton = Button(sidebar, text="Centre Hollywood", command=showHollywoodCenter)
+    actorCentreHollwoodButton.pack(padx=(20),pady=(20))
+
+    #Eloignement Max
+    eloignementMaxButton = Button(sidebar, text="Eloignement Max", command=showMaxDistance)
+    eloignementMaxButton.pack(padx=(20),pady=(20))
 
     sidebar.pack(expand=False, fill='both', side='left', anchor='nw')
 
-def rightPanel(root:Tk):
+def rightPanel():
     mainarea = Frame(root, bg='white', width=500, height=500)
 
      # A compléter
@@ -65,15 +92,16 @@ def rightPanel(root:Tk):
 
 def GUI(): #interface graphique utilisateur
     #A finir
+    global root
     root = Tk()
     root.geometry('1000x500')
     root.title("The Beacon Finder")
-    leftPanel(root)
-    rightPanel(root)
+    leftPanel()
+    rightPanel()
     root.mainloop()
     return root
 
-def afficheGUI(root:Tk):
+def afficheGUI():
     root.mainloop()
 
 
@@ -81,7 +109,7 @@ if __name__ == "__main__" :
     # Initialisation et lancement
     print("Hello World")
     # print(load_json()) #Pour tester si on arrive bien à récuperer le ficher
-    GRAPH = json_ver_nx(load_json()) #charge un fichier choisi par l'utilisateur et le transforme en Graph Nx
+    # GRAPH = json_ver_nx(load_json()) #charge un fichier choisi par l'utilisateur et le transforme en Graph Nx
     print(GRAPH)
     afficheGUI(GUI()) #Affiche l'interface graphique
 
